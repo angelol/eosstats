b03bb4999aec512814650ab5ea224e58035040d0
Angelo Laub <angelolaub@gmail.com>
2018-11-21 22:31:57 +0100

    add tool to measure nodeos sync performance

---
+++ measure_sync.js
@@ -0,0 +1,30 @@
+import Eos from 'eosjs-api'
+
+var httpEndpoint;
+if(process.argv[2]) {
+  httpEndpoint = process.argv[2]
+} else {
+  httpEndpoint = 'http://95.216.38.17:8888';  
+} 
+const chainId = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906';
+
+const eos = Eos({httpEndpoint, chainId})
+
+const seconds = 1000
+const minutes = 60*seconds
+const interval = 5*minutes
+
+var a, b;
+function doit() {  
+  eos.getInfo({})
+  .then(info => {
+    b = new Date(info.head_block_time)
+    if(a) {
+      console.log("Speedup factor: ", (b-a)/interval)
+    } 
+    a = b
+    setTimeout(doit, interval)
+  })
+}
+
+doit()
